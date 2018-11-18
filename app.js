0205695ebccb491b6b8d08c7c39997071702b7e7
Janitha <janitharasanga133@gmail.com>
2018-11-19 00:15:39 +0530

    1st router done

--- app.js
+++ app.js
@@ -15,10 +15,22 @@ const route = require('./routes/route');
 //port no
 const port = 3000;
 
+//adding middleware - cors
+app.use(cors());
+
+//body - parser
+app.use(bodyparser.json());
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//routes
+app.use('/api', route);
+
 //testing server
 app.get('/',(req, res)=>{
     res.send('foobar');
-})
+});
 
 app.listen(port,()=>{
     console.log('Server started at port:' + port);
