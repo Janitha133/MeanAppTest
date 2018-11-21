4f6a9167ba5508b1e0b64664ee4c1240b6590575
Janitha <janitharasanga133@gmail.com>
2018-11-21 23:53:59 +0530

    service litle

--- client/src/app/contact.service.ts
+++ client/src/app/contact.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { from } from 'rxjs';
+import 'rxjs/add/operator/map';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class ContactService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  //retrieving ContactService
+  getContacts()
+  {
+    return this.http.get('http://localhost:3000/api/contacts')
+  
+  }
+
 }
