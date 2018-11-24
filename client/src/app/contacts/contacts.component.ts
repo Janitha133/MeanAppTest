361ae79741acf702551e20d9fed015903a2f1408
Janitha <janitharasanga133@gmail.com>
2018-11-24 07:50:33 +0530

    form done

--- client/src/app/contacts/contacts.component.ts
+++ client/src/app/contacts/contacts.component.ts
@@ -17,11 +17,36 @@ export class ContactsComponent implements OnInit {
 
   constructor(private contactService: ContactService) { }
 
+  addContact(){
+    const newContact = {
+      first_name: this.first_name,
+      last_name: this.last_name,
+      phone: this.phone
+    }
+    this.contactService.addContact(newContact)
+      .subscribe((contact: Contact) => {
+        this.contacts.push(contact);
+        this.contactService.getContacts()
+          .subscribe((contcts: Contact[]) => this.contacts = contcts);
+      });
+  }
+
+  deleteContact(id: any){
+    var contacts = this.contacts;
+    this.contactService.deleteContact(id)
+      .subscribe(data => {
+        if(data == 1){
+          for(var i = 0; i < contacts.length; i++){
+            if(contacts[i]._id == id){
+              contacts.splice(i,1)
+            }
+          }
+        }
+      });
+  }
+
   ngOnInit() {
     this.contactService.getContacts()
       .subscribe((contcts: Contact[]) => this.contacts = contcts);
   }
-
-  
-
 }
